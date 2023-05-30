0844a09f1c6bfa76db9abfeb1d1c7e4cec6e609a
Adedolapo Adeagbo <dollypalton@Adedolapos-MacBook-Pro.local>
2023-05-30 08:11:40 +0100

    an accordion list of items

---
+++ src/SingleQuestion.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  const isActive = id === activeId;
+  return (
+    <article className="question">
+      <header>
+        <h5>{title}</h5>
+        <button
+          className="question-btn"
+          onClick={() => toggleQuestion(id)}
+        >
+          {isActive ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {isActive && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default SingleQuestion;
